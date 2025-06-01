a17fa21241d0571693d7d61d51169e2fd754c7b9
Steve <stephen.r.strong@saic.com>
2025-06-01 08:53:46 -0400

    Add Copilot reference guide and register LivingHingeKnowledgeService

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SVGRender.Services;
+using ApprenticeFoundryMentorModeler;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Register traditional hinge service
 builder.Services.AddScoped<LivingHingeService>();
+
+// Register FoundryMentorModeler services
+builder.Services.AddSingleton<IMentorServices, MentorServices>();
+builder.Services.AddSingleton<IMentorModelManager, MentorModelManager>();
+builder.Services.AddSingleton<IMentorPlayground, MentorPlayground>();
+
+// Register our knowledge-based service
+builder.Services.AddScoped<LivingHingeKnowledgeService>();
 
 var app = builder.Build();
 
